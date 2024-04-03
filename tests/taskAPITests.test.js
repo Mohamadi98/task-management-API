d1a909e3c8d67ab7b5283a43843a76bb3a3e0d14
Mohamadi98 <mohamadinaena23@gmail.com>
2024-04-03 12:02:17 +0200

    implementation for additional unit tests for get task by id and create task endpoints

--- tests/taskAPITests.test.js
+++ tests/taskAPITests.test.js
@@ -21,7 +21,7 @@ describe('Testing create tasks endpoint /tasks/:id', () => {
     });
 });
 
-describe('Testing the functionality of create tasks endpoint /tasks/:id in another way', () => {
+describe('Testing the functionality of create tasks endpoint /tasks/:id', () => {
     it('Should Have a status code of 200 and a response property message', async () => {
         const response = await request(app).post('/tasks')
             .send({
@@ -31,5 +31,32 @@ describe('Testing the functionality of create tasks endpoint /tasks/:id in anoth
         const response2 = await request(app).get('/tasks');
         expect(response2.status).toBe(200);
         expect(response2.body.data.length).toBeGreaterThan(0);
+    });
+});
+
+describe('Testing get task by id /tasks/:id', () => {
+    it('Should have a status code of 404', async () => {
+        const response = await request(app).get('/tasks/123');
+        expect(response.status).toBe(404);
+    });
+});
+
+describe('Testing the functionality of create tasks endpoint /tasks/:id', () => {
+    it('Should Have a status code of 400', async () => {
+        const response = await request(app).post('/tasks')
+            .send({
+                title: 'test title',
+            });
+        expect(response.status).toBe(400);
+    });
+});
+
+describe('Testing the functionality of create tasks endpoint /tasks/:id', () => {
+    it('Should Have a status code of 400', async () => {
+        const response = await request(app).post('/tasks')
+            .send({
+                description: 'test description'
+            });
+        expect(response.status).toBe(400);
     });
 });
